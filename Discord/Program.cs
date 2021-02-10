690eda4b4f25fc559d7b3f5a23b9a1b0765b5e55
niko391a <69769473+niko391a@users.noreply.github.com>
2021-02-10 15:24:55 +0100

    2.0
    With help from Bo

--- Discord/Program.cs
+++ Discord/Program.cs
@@ -22,26 +22,20 @@ namespace Discord
             {
                 try //error handling
                 {
-                //    string[] ports;
-                //    ports = new string[8] { "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", };
 
-                //    string[] portNames = SerialPort.GetPortNames();
-                //    foreach (array_combine (portName, portNames) as port => ports);
-                //    {
-                //        SerialPort port = new SerialPort(portName, 9600); //creates a new instance of the SerialPort Class
-                //        port.Open();
-                //    }
+                    SerialPort Srp = new SerialPort(); //creates a new instance of the SerialPort Class
+                    string[] ports = SerialPort.GetPortNames(); //to ensure that it reads from the right serial port the following will be used
+                    Srp.PortName = ports[ports.Length - 1]; //we will use this because the arduino is usually connected to the highest numbered COM port 
+                    Srp.BaudRate = 9600; //bits per second
+                    Srp.Open(); //opens the COM port
 
-                    SerialPort Srp = new SerialPort("COM4", 9600); //creates a new instance of the SerialPort Class
-                    Srp.Open();
-
-                    int input = Srp.ReadChar(); //reads incoming siganl from serial
+                    int input = Srp.ReadChar(); //reads incoming siganl from serialport
                     if (input >= 0) //if a signal is read then it will execute the following code
                     {
                         Console.WriteLine("Distress signal detected"); //output to user that the signal has been detected
                         var proc = new Process(); //creates a new instance of the Process class
                         proc.StartInfo.FileName = $@"{parentFolder}\\Discord Arduino Comms App.exe"; //sets the path to the discord comms app
-                        proc.StartInfo.Arguments = "-v -s -a"; 
+                        proc.StartInfo.Arguments = "-v -s -a";
                         proc.Start(); //starts the application
                         proc.WaitForExit(); //instruct the process to wait indefinitely for the asssociated process to exit
                         var exitCode = proc.ExitCode;
